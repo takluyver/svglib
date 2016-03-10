c293c3f2be6927ae76d9076d1430cdfd0d3a4ada
Sebastian Wehrmann <sw@gocept.com>
2016-03-10 08:14:08 +0100

    Make this svglib compatible with python 3.

--- setup.py
+++ setup.py
@@ -1,20 +1,60 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+"""SVGLIB"""
 
+from setuptools import setup, find_packages
+import glob
+import os.path
 import sys
-sys.path.insert(0, "src")
-
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup
 
 
-import metainfo
+def project_path(*names):
+    return os.path.join(os.path.dirname(__file__), *names)
 
-# get module not starting with '_'
-# this also excludes '__builtins__', '__doc__', '__file__', '__name__'
-items = dict([(k, metainfo.__getattribute__(k)) 
-    for k in metainfo.__dict__ if k[0] != '_'])
 
-# pass them to distutils
-setup(**items)
+def read(path):
+    if sys.version_info < (3,):
+        f = open(path)
+    else:
+        f = open(path, encoding='UTF-8')
+    text = f.read()
+    f.close()
+    return text
+
+setup(
+    name='svglib',
+    version='0.7.0',
+
+    install_requires=[
+    ],
+
+    author='Dinu Gherman',
+    author_email='',
+    license='LGPL 3',
+
+    keywords='',
+    classifiers="""\
+        Development Status :: 4 - Beta
+        Environment :: Console
+        Intended Audience :: Developers
+        Intended Audience :: End Users/Desktop
+        License :: OSI Approved :: GNU General Public License (GPL)
+        Natural Language :: English
+        Operating System :: MacOS :: MacOS X
+        Operating System :: Microsoft :: Windows
+        Operating System :: POSIX
+        Programming Language :: Python
+        Topic :: Documentation
+        Topic :: Multimedia :: Graphics :: Graphics Conversion
+        Topic :: Printing
+        Topic :: Software Development :: Libraries :: Python Modules
+        Topic :: Text Processing :: Markup :: XML
+        Topic :: Utilities
+"""[:-1].split('\n'),
+    description="An experimental library for reading and converting SVG.",
+    long_description='\n\n'.join(read(project_path(name)) for name in (
+        'README.txt',
+    )),
+
+    include_package_data=True,
+    data_files=[('', glob.glob(project_path('*.txt')))],
+    zip_safe=False,
+)
